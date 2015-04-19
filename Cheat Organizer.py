73b42d52a0ffdcfcb7e44dfa49c896c35225226c
Grohden <gabriel_rodhem@hotmail.com>
2015-04-19 18:20:23 -0300

    Update
    At this point the code should be able to get the cheats (without the
    cheat name) from one site.

--- Cheat Organizer.py
+++ Cheat Organizer.py
@@ -7,20 +7,42 @@ class Source:
         def __init__(self, url):
                 self.URL = url
         
-        def get_source(self):
-                return urllib.request.urlopen(self.URL).read().decode(encoding='utf-8',errors='ignore') #it needs the decode, to use find()
+        def getSource(self):
+                #it needs the decode, to use find()
+                return urllib.request.urlopen(self.URL).read().decode(encoding='utf-8',errors='ignore')
 
-        def get_cheat(self):
-                texto=self.get_source()
-                start_tag = '<td class="cheats right">'
-                end_tag='</td>'
-                comeco = texto.find(start_tag,len(start_tag))            
-                fim = texto.find(end_tag,comeco+len(end_tag))                              
-                url =texto[comeco:fim+len(end_tag)]
+        def getCheat(self):
+                texto=self.getSource()
+                counter=texto.find('<')
+                url=''
+                while counter != -1:
+                    start_tag = '<td class="cheats right">'
+                    end_tag = '</td>'
+                    start = texto.find(start_tag,counter)
+                    end = texto.find(end_tag,counter)
+                    url += texto[start:end]
+                    counter=texto.find('<',counter+1)
+
                 
-                print("Start char number in text: ",comeco)
-                print("End char number in text: ",fim)
-                print("Text chars number: ",len(url))
-                print("Text from start to end: ",url)
-                
+                #print("Start char number in text: ",start)
+                #print("End char number in text: ",end)
+                #print("Text chars number: ",len(url))
+                #print("Text from start to end: ",url)
+
+                url= url.replace("<td class=\"cheats right\">", "\nCheat:\n")
+                url= url.replace("<br />", "\n")
+                return url
+
+        def removeTags(self,string):
+            counter = len(string)
+            while counter >= len(string):
+                start = string.find('<', counter)
+                end = string.find('>', counter)
+                string += string[start:end+1]
+                counter = end
+                print(counter)
+            return string
+
+s=Source('http://cheats.codetwink.com/ps2/view/984')
+print(s.getCheat()) #at this point i'm able to get all cheats from the site
 
