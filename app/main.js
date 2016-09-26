68ef3d0be32126e587c773b0902ee2f4eb68eba8
Grohden <gabriel_rohden@outlook.com>
2016-09-26 15:01:02 -0300

    Partial  cog position.

--- app/main.js
+++ app/main.js
@@ -11,4 +11,35 @@
             location.reload();
         }
     });
+
+    //makes the opened cog be always at its half on the screen
+    $(window).resize(function(){
+        putHalfCogAtLeft();
+    });
+
+    $('.cog-opened img').ready(function(){
+        putHalfCogAtLeft();
+    });
 }());
+
+
+var putHalfCogAtLeft = function putHalfCogAtLeft() {
+    var cog = $('.cog-opened img')[0];
+    if (!cog) return;
+
+    var dimens = cog.getBoundingClientRect();
+    writeInStyleTag('.cog-opened img',{left:-(dimens.width / 2)+'px'});
+    cog.style.marginLeft = 0;
+};
+
+//rewrites style tag. style tag loses its content when this function is called.
+var writeInStyleTag = function writeInSyleTag(classOrId, obj = {}) {
+    if(!classOrId) return;
+    var str="\n"+classOrId+"{";
+    for(var x=0; x<Object.keys(obj).length;x++){
+        str+="\n"+Object.keys(obj)[x]+":"+obj[Object.keys(obj)[x]]+";";
+    }
+    str+="\n}";
+    //override content
+    $('#main-style').text(str);
+};
